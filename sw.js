9d08a73509638236c134e73c654bf6cff95ba2f6
el3omdaz <70915479+el3omdaz@users.noreply.github.com>
2026-06-24 14:46:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "ideaspark-v120";
-const ASSETS = ["/", "/index.html", "/manifest.json"];
+const CACHE = "ideaspark-full-v122-splash-inline";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
